42a238a7eba499cd9d61804157151c671a0a337a
Fabricio Lima <fabriciolima31@gmail.com>
2015-11-17 18:52:27 +0000

    Teste

--- WebService/include/Config.php
+++ WebService/include/Config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'task_manager');
 
